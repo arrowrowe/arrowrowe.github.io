7d27f73a945959d1f50be9a1dc33f5e71d4dabc4
arrowrowe <arrowrowe@gmail.com>
2015-11-13 22:43:00 +0800

    Add Feed

--- blog/general/archive.md
+++ blog/general/archive.md
@@ -4,8 +4,8 @@ All, [中文](#!/archive.chs), [English](#!/archive.en)
 
 ### 2015
 
-- Nov 07, [Awesome Atom Packages and More](#!/blog/2015/awesome-atom-packages-and-more)
-- Oct 31, [重启](#!/blog/2015/restart)
-- May 02, [RSS 清单](#!/blog/2015/my-rss-subscriptions)
-- March 29, [dyTopic: Git - Getting Started](#!/blog/2015/git-getting-started-for-dy)
-- Mar 26, [dyTopic](#!/blog/2015/dy-topic)
+- 2015/11/07, [Awesome Atom Packages and More](#!/blog/2015/awesome-atom-packages-and-more): Atom packages recommended for developers.
+- 2015/10/31, [重启](#!/blog/2015/restart): 重新实现了一遍博(markdown)客(previewer)...
+- 2015/05/02, [RSS 清单](#!/blog/2015/my-rss-subscriptions): 不定期更新.
+- 2015/03/29, [dyTopic: Git - Getting Started](#!/blog/2015/git-getting-started-for-dy): A Git lecture for beginners.
+- 2015/03/26, [dyTopic](#!/blog/2015/dy-topic): A list of dyTopics.
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
+var fs = require('fs-extra');
+
+gulp.task('publish', ['build', 'feed']);
 
 /******* Tam's Building ***************************************/
 var tam = require('tam');
-var fs = require('fs-extra');
 var tamHTML = require('tam-html');
 var assetsPath = './assets.json';
 
@@ -15,4 +17,48 @@ gulp.task('build', ['clean'], function () {
 
 gulp.task('watch', ['build'], function () {
   gulp.watch('{src,assets}/**/*', ['build']);
+});
+
+/******* Feed *************************************************/
+var RSS = require('rss');
+
+gulp.task('feed', function () {
+
+  var site_url = 'http://arrowrowe.me/';
+  var author = 'arrowrowe <arrowrowe@gmail.com>';
+
+  var feed = new RSS({
+    title: 'Flying yet Motionless | arrowrowe',
+    description: 'Blog of arrowrowe.',
+    feed_url: site_url + 'feed',
+    site_url: site_url,
+    image_url: site_url + 'logo.png',
+    managingEditor: author,
+    webMaster: author,
+    copyright: 'http://creativecommons.org/licenses/by-nc-sa/4.0/',
+    ttl: 40
+  });
+
+  match(
+    fs.readFileSync('./blog/general/archive.md', 'utf8'),
+    /- (.+?), \[(.+?)\]\((.+?)\): (.+)/g,
+    function (content, time, title, hash, description) {
+      feed.item({
+        title: title,
+        description: description,
+        url: site_url + hash,
+        guid: hash.substr(8),
+        date: time
+      });
+    }
+  );
+
+  fs.writeFileSync('./feed', feed.xml());
+
+  function match(text, re, fn) {
+    var m;
+    while ((m = re.exec(text)) !== null) {
+      fn.apply(null, m);
+    }
+  }
 });
