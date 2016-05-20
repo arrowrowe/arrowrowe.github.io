5ee20b8f2ef43c754058234d31bca41e1678d06e
arrowrowe <arrowrowe@gmail.com>
2016-05-20 09:40:10 +0800

    :boom: introduce mathjax
    - Introduce a script loader, usage: `<!-- script: mathjax -->`.
    - Match all the syntax above as scripts by regexp.
    - Use Promise to ensure Markdown rendering and scripts' loading.
    
    However, bugs exist:
    - MathJax fails to recogonize some pairs.
    - Rendered results are too small (automatically set to 50% font
    size, don't know why).

--- assets/index.js
+++ assets/index.js
@@ -17,6 +17,43 @@
     });
   };
 
+  var matchAll = function (pattern, src) {
+    var match = null;
+    var matches = [];
+    while (match = pattern.exec(src)) {
+      matches.push(match);
+    }
+    return matches;
+  };
+  const identity = function (x) { return x; };
+
+  var scriptLoaders = {
+    mathjax: {
+      step: 0,
+      load: function () {
+        if (this.step === 0) {
+          this.step = 1;
+          return new Promise(function (resolve) {
+            $.getScript("https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_CHTML", resolve);
+          });
+        }
+      },
+      render: function () {
+        if (this.step === 1) {
+          this.step = 2;
+          MathJax.Hub.Config({
+            tex2jax: {
+              inlineMath: [['($','$)']],
+              displayMath: [['[$','$]']]
+            },
+          });
+        } else {
+          MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+        }
+      }
+    }
+  };
+
 
   marked.setOptions({
     highlight: function (code, lang, callback) {
@@ -38,15 +75,44 @@
     '/:nav': '/blog/general/:nav'
   });
   router.afterEach(function (transition) {
-    $.get(transition.to.path.substr(1) + '.md', function (raw) { marked(raw, function (err, content) {
-      router.app.content = content;
-      Vue.nextTick(function () {
-        optionDisqus.path = transition.to.path;
-        optionDisqus.title = $('h1').text();
-        document.title = optionDisqus.title + ' | arrowrowe';
-        initDisqus();
-      });
-    }); });
+    $.get(transition.to.path.substr(1) + '.md', function (raw) {
+
+      var scripts = matchAll(/^ *<!-- *script: *(.+?) *-->$/mig, raw).map(function (match) {
+        return scriptLoaders[match[1].toLowerCase()];
+      }).filter(identity);
+
+      Promise.all([
+        new Promise(function (resolve, reject) {
+          marked(raw, function (err, content) {
+            if (err) {
+              reject(err);
+              return;
+            }
+            router.app.content = content;
+            Vue.nextTick(resolve);
+          });
+        })
+      ].concat(scripts.map(function (script) {
+        return script.load();
+      })))
+        .then(function () {
+          console.log('scripts loaded');
+          optionDisqus.path = transition.to.path;
+          optionDisqus.title = $('h1').text();
+          document.title = optionDisqus.title + ' | arrowrowe';
+          initDisqus();
+          return Promise.all(scripts.map(function (script) {
+            return script.render();
+          }));
+        })
+        .then(function () {
+          console.log('scripts rendered');
+        })
+        .catch(function (err) {
+          console.warn(err);
+        });
+
+    });
   });
   router.start(Vue.extend({
     data: function () {
---
+++ blog/2016/project-euler-541.md
@@ -0,0 +1,17 @@
+Project Euler 541 (A Failed Attempt)
+===
+
+(Spend a day and then give up...)
+
+Name that
+  [$ lcm_n=lcm(1,\cdots,n) $]
+obviously
+  [$ b_n\mid lcm_n $]
+thus
+  [$ p\nmid b_{p-1} $]
+which suggests that
+  [$ M(p)>p-1 $]
+or equivalently
+  [$ M(p)\ge p $]
+
+<!-- Script: MathJax -->
